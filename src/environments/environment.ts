68c640df35e0ae5e4f55d6fbbd4d02c2206cd9d1
M-AlMokadem <moh_skimo@hotmail.com>
2021-09-30 13:35:07 +0200

    adding environment vars - query service - item service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'http://localhost:5001/',
+  apiUrls: {
+    getAllItems: 'api/Items/GetAllItems'
+  },
 };
 
 /*
